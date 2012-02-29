ae2338411b2b53c243aab62bfcfe8c1d552ecc81
Andrew J Cronk <andy@tempo-db.com>
2012-02-29 13:21:39 -0600

    working example to display series

--- web.rb
+++ web.rb
@@ -1,5 +1,15 @@
 require 'sinatra'
+require 'tempodb'
 
 get '/' do
-  "Hello, world"
+  api_key = ENV['TEMPODB_API_KEY']
+  api_secret = ENV['TEMPODB_API_SECRET']
+  api_host = ENV['TEMPODB_API_HOST']
+  api_port = Integer(ENV['TEMPODB_API_PORT'])
+  api_secure = ENV['TEMPODB_API_SECURE'] == "False" ? false : true
+
+  client = TempoDB::Client.new( api_key, api_secret, api_host, api_port, api_secure )
+  out = ""
+  client.get_series().each{ |series| out += series.to_json + "<br/>"  }
+  out
 end
